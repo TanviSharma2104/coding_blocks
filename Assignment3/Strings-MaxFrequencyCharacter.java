18fbc483476bbbb38f05d78b24fc4cd4f850a1b3
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-05-17 15:48:08 +0530

    Create Strings-Max Frequency Character

---
+++ Assignment3/Strings-MaxFrequencyCharacter.java
@@ -0,0 +1,51 @@
+// Take as input S, a string. Write a function that returns the character with maximum frequency. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// A string of length between 1 to 1000.
+
+// Output Format
+// Character
+
+// Sample Input
+// aaabacb
+// Sample Output
+// a
+// Explanation
+// For the given input string, a appear 4 times. Hence, it is the most frequent character.
+
+import java.util.*;
+public class Main {
+	static final int ASCII_SIZE = 256;
+    public static void main(String args[]) {
+        // Your Code Here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+
+		int count[] = new int[ASCII_SIZE];
+ 
+        // Construct character count array from the input
+        // string.
+        int len = str.length();
+        for (int i = 0; i < len; i++)
+            count[str.charAt(i)]++;
+ 
+        int max = -1; // Initialize max count
+        char result = ' '; // Initialize result
+ 
+        // Traversing through the string and maintaining
+        // the count of each character
+        for (int i = 0; i < len; i++) {
+            if (max < count[str.charAt(i)]) {
+                max = count[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+		System.out.print(result);
+ 
+        
+
+    }
+}
