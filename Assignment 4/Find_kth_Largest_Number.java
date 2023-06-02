8df9ae7c39562f8ebbbb309584a3b9b11fe50acb
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-06-02 19:55:16 +0530

    Create Find_kth_Largest_Number

---
+++ Assignment 4/Find_kth_Largest_Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main (String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		System.out.print(arr[n-k]);
+    }
+}
